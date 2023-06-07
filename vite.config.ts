8bbc41690bb39ddacbef8a168bf3a5634cb0bb63
Thomas <nyumat18@gmail.com>
2023-06-07 01:40:55 -0700

    Leap of Faith

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss("./tailwind.config.js")],
+    },
+  },
   assetsInclude: [
     "**/*.jpg",
     "**/*.JPG",
