45ced8f8f349f0f0e7ab1c423be94f7f6b3a5c9f
Thomas <nyumat18@gmail.com>
2023-06-09 17:02:44 -0700

    Trailing Cursor Feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import Scrollbar from "./components/animations/Scrollbar"
 import Hero from "./components/content/Hero"
 import Main from "./components/layout/Main"
-
+import AnimatedCursor from "react-animated-cursor"
 function App() {
 
   return (
     <>
+      <AnimatedCursor showSystemCursor={false}
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={1.7}
+        outerAlpha={0}
+        outerStyle={{
+          border: '3px solid #fff',
+        }}
+        innerStyle={{
+          backgroundColor: '#fff',
+        }}
+      />
       <Scrollbar />
       <Hero />
       <Main />
