dbaf0e2241e83794be8def23b6aea8252354f98e
Thomas <nyumat18@gmail.com>
2023-12-26 17:56:28 -0800

    🍊 semi-colorful hero

--- src/components/content/Hero.tsx
+++ src/components/content/Hero.tsx
@@ -1,52 +1,53 @@
-import { useState, useEffect } from 'react';
-import Typewriter from '../text/TypewriterEffect';
-import '../../index.css'
-import { heroImages } from '../../data';
-import { Image } from '../../types';
-
+import { useEffect, useState } from "react";
+import { heroImages } from "../../data";
+import "../../index.css";
+import { Image } from "../../types";
+import Typewriter from "../text/TypewriterEffect";
 
 const Hero = () => {
-      const [currentImageIndex, setCurrentImageIndex] = useState(0);
-      const images: Image[] = heroImages
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images: Image[] = heroImages;
 
-      useEffect(() => {
-            const interval = setInterval(() => {
-                  setCurrentImageIndex((prevIndex) =>
-                        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-                  );
-            }, 3500);
-
-            return () => clearInterval(interval);
-      }, [images.length]);
-
-      return (
-            <div className="relative w-full h-screen">
-                  {images.map((image: Image, index: number) => {
-                        return (
-                              <div
-                                    key={image.id}
-                                    className={`absolute top-0 left-0 w-full h-full transition-opacity ${index === currentImageIndex ? 'opacity-100' : 'opacity-0'}`}
-                                    style={{
-                                          backgroundImage: `url(${image.image})`,
-                                          backgroundSize: 'cover',
-                                          backgroundPosition: 'center',
-                                          transition: 'opacity 1s ease-in-out',
-                                    }}
-                              >
-                                    <div className="absolute top-0 left-0 w-full h-full bg-black opacity-[65%]" />
-                              </div >
-                        );
-                  })}
-                  <div className="flex flex-col justify-center items-center w-full h-full opacity-100">
-                        <Typewriter />
-                        <div className="justify-center transform translate-y-60 mt-32">
-                              <div className="arrow bounce"></div>
-                              <div className="arrow bounce"></div>
-                              <div className="arrow bounce"></div>
-                        </div>
-                  </div>
-            </div >
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
+    }, 3500);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  return (
+    <div className="relative w-full h-screen">
+      {images.map((image: Image, index: number) => {
+        return (
+          <div
+            key={image.id}
+            className={`absolute top-0 left-0 w-full h-full transition-opacity ${
+              index === currentImageIndex ? "opacity-100" : "opacity-0"
+            }`}
+            style={{
+              backgroundImage: `url(${image.image})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              transition: "opacity 1s ease-in-out",
+            }}
+          >
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-[65%]" />
+          </div>
+        );
+      })}
+      <div className="flex flex-col justify-center items-center w-full h-full opacity-100 ">
+        <Typewriter />
+        <div className="justify-center transform translate-y-60 mt-32">
+          {/* <div className="arrow bounce"></div> */}
+          {/* <div className="arrow bounce"></div> */}
+          <div className="arrow bounce"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/text/TypewriterEffect.tsx
+++ src/components/text/TypewriterEffect.tsx
@@ -1,35 +1,49 @@
-import { useEffect, useRef } from 'react';
-import Typewriter from 'typewriter-effect/dist/core';
+import { useEffect, useRef } from "react";
+import Typewriter from "typewriter-effect/dist/core";
 
 function TypewriterComponent() {
-      const elementRef = useRef(null);
-      let typewriter: { stop: () => void; };
+  const elementRef = useRef(null);
+  let typewriter: { stop: () => void };
 
-      useEffect(() => {
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-            typewriter = new Typewriter(elementRef.current, {
-                  strings: ['I am a <strong>Web Developer.</strong>', '<strong>  Student.</strong>', '<strong>Musician.</strong>', '<strong>Friend.</strong>', '<strong>Designer.</strong>', '<strong>Software Engineer.</strong>'],
-                  autoStart: true,
-                  loop: true,
-                  delay: 50,
-            });
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    typewriter = new Typewriter(elementRef.current, {
+      strings: [
+        "I build <strong>web apps.</strong>",
+        "I am a <strong>student.</strong>",
+        "<strong>Musician.</strong>",
+        "<strong>Person of color.</strong>",
+        "<strong>Designer.</strong>",
+        "<strong>Software Engineer.</strong>",
+      ],
+      autoStart: true,
+      loop: true,
+      delay: 50,
+      cursor: "|",
+      cursorClassName:
+        "text-orange-600 saturate-200 animate-[pulse_1s_ease-in-out_infinite] font-bold",
+      deleteSpeed: 45,
+    });
 
-            return () => {
-                  if (typewriter) {
-                        typewriter.stop();
-                  } else {
-                        return;
-                  }
-            };
-      }, []);
+    return () => {
+      if (typewriter) {
+        typewriter.stop();
+      } else {
+        return;
+      }
+    };
+  }, []);
 
-      return (
-            <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-5xl whitespace-wrap text-center z-50 pb-16 w-full text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                  Hi! I'm Tom Nyuma.
-                  <span className="text-accent"> | </span>
-                  <span ref={elementRef}></span>
-            </h1>
-      );
+  return (
+    <div className="mx-16 xs:mx-8 sm:mx-12 md:mx-16 lg:mx-24 xl:mx-32 2xl:mx-48">
+      <h1 className="text-4xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl 2xl:text-7xl whitespace-wrap text-center z-50 pb-16 w-full text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        Hey, I'm{" "}
+        <span className="text-orange-600 saturate-200">Tom Nyuma!</span>{" "}
+        <span className="text-slate-500/50"> | </span>
+        <span ref={elementRef}></span>
+      </h1>
+    </div>
+  );
 }
 
 export default TypewriterComponent;
