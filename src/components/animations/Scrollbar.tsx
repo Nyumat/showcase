90c93a759b3c104b3f5bc3f35a36dc95ef418e23
Thomas <nyumat18@gmail.com>
2023-12-26 17:54:54 -0800

    😴 formater goes brrrr

--- src/components/animations/Scrollbar.tsx
+++ src/components/animations/Scrollbar.tsx
@@ -1,12 +1,15 @@
-import { motion, useScroll } from "framer-motion"
-import { FC } from "react"
+import { motion, useScroll } from "framer-motion";
+import { FC } from "react";
 
 const Scrollbar: FC = (): JSX.Element => {
-      const { scrollYProgress } = useScroll();
-      // add "tranform origin-left" to make the scrollbar grow from the left
-      return (
-            <motion.div style={{ scaleX: scrollYProgress }} className="fixed top-0 left-0 w-full h-2 bg-orange-500 z-50"></motion.div>
-      )
-}
+  const { scrollYProgress } = useScroll();
+  // add "tranform origin-left" to the the scrollbar grow from the left
+  return (
+    <motion.div
+      style={{ scaleX: scrollYProgress }}
+      className={`fixed top-0 left-0 w-full h-2 bg-orange-600 z-50`}
+    ></motion.div>
+  );
+};
 
-export default Scrollbar
+export default Scrollbar;
