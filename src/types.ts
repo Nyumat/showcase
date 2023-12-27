743f62a2750657290a975990c9c21eda640ee864
Thomas <nyumat18@gmail.com>
2023-12-26 17:50:10 -0800

    🕺🏾 removed shuffle effect and arrow

--- src/types.ts
+++ src/types.ts
@@ -1,60 +1,59 @@
 export type Project = {
-      id: number | string;
-      title: string;
-      image: string;
-      link?: string;
-      github?: string;
-}
+  id: number | string;
+  title: string;
+  image: string;
+  link?: string;
+  github?: string;
+};
 
 export type Image = {
-      id: number | string;
-      title?: string;
-      image: string;
-      link?: string;
-}
+  id: number | string;
+  title?: string;
+  image: string;
+  link?: string;
+};
 
 export type Interest = {
-      id: number | string;
-      title: string;
-      image: string;
-      link?: string;
-}
+  id: number | string;
+  title: string;
+  image: string;
+  link?: string;
+};
 
 export type Skill = {
-      id: number;
-      title: string;
-      image: string;
-      link?: string;
-}
+  id: number;
+  title: string;
+  image: string;
+  link?: string;
+};
 
 export type Experience = {
-      id: number;
-      title: string;
-      image: string;
-      position?: string;
-      date?: string;
-      description?: string;
-}
+  id: number;
+  title: string;
+  image: string;
+  position?: string;
+  date?: string;
+  description?: string;
+};
 
 export type Education = {
-      id: number;
-      title: string;
-      image: string;
-      date?: string;
-}
+  id: number;
+  title: string;
+  image: string;
+  date?: string;
+};
 
 export type Contact = {
-      id: number;
-      title: string;
-      image: string;
-      link?: string;
-}
+  id: number;
+  title: string;
+  image: string;
+  link?: string;
+};
 
 export type Social = {
-      id: number;
-      title: string;
-      image: string;
-      link?: string;
-}
-
-
+  id: number;
+  title: string;
+  image: string;
+  link?: string;
+  username?: string;
+};
