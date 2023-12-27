b84e567b7662707d06570e372dc0abd796426ec2
Thomas <nyumat18@gmail.com>
2023-12-26 17:51:44 -0800

    ✨ moved project title above image

--- src/components/animations/ProjectCard.tsx
+++ src/components/animations/ProjectCard.tsx
@@ -1,69 +1,77 @@
-import { motion, AnimatePresence, useAnimation } from "framer-motion";
-import { Project } from '../../types';
+import { AnimatePresence, motion, useAnimation } from "framer-motion";
 import { ReactElement, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
+import { Project } from "../../types";
 
 interface ProjectCardProps {
-      projects: Project[];
+  projects: Project[];
 }
 
 const ProjectCard = ({ projects }: ProjectCardProps): ReactElement => {
-      const control = useAnimation();
-      const [ref, inView] = useInView();
+  const control = useAnimation();
+  const [ref, inView] = useInView();
 
-      const staggerDelay = 0.30;
+  const staggerDelay = 0.3;
 
-      useEffect(() => {
-            if (inView) {
-                  control.start("visible");
-            }
-      }, [control, inView]);
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    }
+  }, [control, inView]);
 
-      return (
-            <div className="flex flex-col justify-center items-center w-full h-full opacity-100 my-16">
-                  <AnimatePresence>
-                        <motion.div
-                              initial="hidden"
-                              animate={control}
-                              variants={{
-                                    visible: {
-                                          transition: { staggerChildren: staggerDelay },
-                                    },
-                              }}
-                              className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8"
-                              ref={ref}
-                        >
-                              {projects.map((project: Project) => (
-                                    <motion.div
-                                          key={project.id}
-                                          variants={{
-                                                hidden: { opacity: 0, y: 20 },
-                                                visible: { opacity: 1, y: 0 },
-                                          }}
-                                          className="relative w-full h-full px-16"
-                                    >
-
-                                          <div className="flex flex-col" onClick={() => window.open(project.link)}>
-                                                <img
-                                                      src={project.image}
-                                                      alt={project.title}
-                                                      className="w-full h-full rounded-lg shadow-lg hover:shadow-2xl transition duration-500 ease-in-out transform hover:scale-110 hover:cursor-pointer"
-                                                />
-                                                <i className="text-xs text-center text-white align-center justify-center mt-4 hover:cursor-pointer hover:text-blue-300 hover:underline" onClick={() => window.open(project.link)}>
-                                                      {project.link}
-                                                      <img src="https://img.icons8.com/ios-filled/50/ffffff/github.png" className="inline-block w-6 h-6 ml-2 cursor-pointer" onClick={() => window.open(project.github)} />
-                                                </i>
-                                                <h2 className="mt-4 text-2xl font-bold text-center text-white">
-                                                      {project.title}
-                                                </h2>
-                                          </div>
-
-                                    </motion.div>
-                              ))}
-                        </motion.div>
-                  </AnimatePresence>
-            </div>
-      );
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full opacity-100 my-16">
+      <AnimatePresence>
+        <motion.div
+          initial="hidden"
+          animate={control}
+          variants={{
+            visible: {
+              transition: { staggerChildren: staggerDelay },
+            },
+          }}
+          className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8"
+          ref={ref}
+        >
+          {projects.map((project: Project) => (
+            <motion.div
+              key={project.id}
+              variants={{
+                hidden: { opacity: 0, y: 20 },
+                visible: { opacity: 1, y: 0 },
+              }}
+              className="relative w-full h-full px-16"
+            >
+              <div
+                className="flex flex-col"
+                onClick={() => window.open(project.link)}
+              >
+                <h2 className="my-4 text-2xl font-bold text-center text-white">
+                  {project.title}
+                </h2>
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full rounded-lg transition duration-500 ease-in-out transform hover:scale-110 hover:cursor-pointer border-2 border-slate-600/30"
+                />
+                <i
+                  className="text-xs text-center text-white align-center justify-center mt-4 hover:cursor-pointer hover:text-blue-300 hover:underline"
+                  onClick={() => window.open(project.link)}
+                >
+                  {project.link}
+                  <img
+                    src="https://img.icons8.com/ios-filled/50/ffffff/github.png"
+                    className="inline-block w-6 h-6 ml-2 cursor-pointer"
+                    onClick={() => window.open(project.github)}
+                  />
+                </i>
+              </div>
+            </motion.div>
+          ))}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
 };
 
 export default ProjectCard;
