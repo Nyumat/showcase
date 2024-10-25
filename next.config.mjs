06aac929127b0a21fcc4eaf9316b95dce7597f70
Thomas <nyumat18@gmail.com>
2024-10-25 16:11:28 -0700

    🫠 unoptimized images because i will not pay

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,19 @@
-import mdx from '@next/mdx';
-import createNextIntlPlugin from 'next-intl/plugin';
+import mdx from "@next/mdx";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withMDX = mdx({
-    extension: /\.mdx?$/,
-    options: { },
+  extension: /\.mdx?$/,
+  options: {},
 });
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextIntl(withMDX(nextConfig));
