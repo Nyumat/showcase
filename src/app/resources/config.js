4d170cb4b416dc30159bf97e4afb0b58686eeef1
Thomas <nyumat18@gmail.com>
2024-10-25 15:04:45 -0700

    🗣️ i8n 'en' strings and content

---
+++ src/app/resources/config.js
@@ -0,0 +1,58 @@
+const baseURL = 'tomnyuma.rocks';
+
+// Enable localization
+const i18n = false;
+
+// Manage localized content in the messages folder
+const i18nOptions = {
+    locales: ['en', 'fr'],            // A list of all locales that are supported, e.g. ['en','id']
+    defaultLocale: 'en'         // Locale used by default and as a fallback
+}
+
+const routes = {
+    '/':        true,
+    '/about':   true,
+    '/work':    true,
+    '/blog':    true,
+    '/gallery': true,
+}
+
+// Enable password protection on selected routes
+// Set password in pages/api/authenticate.ts
+const protectedRoutes = {
+    '/work/automate-design-handovers-with-a-figma-to-code-pipeline': true
+}
+
+const effects = {
+    gradient: true,
+    dots:     true,
+    lines:    false,
+}
+
+const style = {
+    theme:       'dark',         // dark | light
+    neutral:     'gray',         // sand | gray | slate
+    brand:       'aqua',         // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    accent:      'yellow',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    solid:       'contrast',     // color | contrast
+    solidStyle:  'flat',         // flat | plastic
+    border:      'playful',      // rounded | playful | conservative
+    surface:     'translucent',  // filled | translucent
+    transition:  'all'           // all | micro | macro
+}
+
+const display = {
+    location: true,
+    time:     true,
+}
+
+const mailchimp = {
+    action: 'https://url/subscribe/post?parameters',
+    effects: {
+        gradient: true,
+        dots:     false,
+        lines:    true,
+    }
+}
+
+export { routes, protectedRoutes, effects, style, display, mailchimp, baseURL, i18n, i18nOptions };
